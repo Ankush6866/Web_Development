59614f2e69b86d5f530ddaeecde4b7dab2ee24f6
Ankush6866 <ankushprof875@gmail.com>
2025-05-22 18:03:34 +0530

    Added Lecture_7

---
+++ Lecture_7/1.js
@@ -0,0 +1,23 @@
+let string = 'this is nirbhay\'s string'
+
+let newstring = `this is nirbhay's string`
+
+
+//templates
+
+let p = 'Nirbhay'
+
+let str= `Hi i am ${p}`
+
+let longStr='this is \
+a very long\
+string'
+
+let s = 'this has some data here'
+let key = 'has'
+console.log(s.length)
+console.log(s.indexOf(key))
+console.log(s.indexOf('is'))
+console.log(s.indexOf('is',3))
+
+
